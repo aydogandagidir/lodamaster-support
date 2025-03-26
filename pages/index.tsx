1275098e805c4ca3ed2ed6c402b01d1e22890ee6
Aydoğan Dağıdır <152778304+aydogandagidir@users.noreply.github.com>
2025-03-26 16:22:07 +0300

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Lodamaster Support App</h1>
+      <p>Welcome! Please <a href="/login" style={{ color: "blue" }}>Login here</a>.</p>
+    </div>
+  );
+}
